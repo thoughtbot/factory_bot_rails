c4eab793e3198cf6f0796fbfbb0215a70c137752
Mike Burns <mburns@thoughtbot.com>
2011-07-12 09:50:31 -0400

    Extend the Aruba timeout to an hour because bundler takes too long.

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,5 +3,5 @@ require 'aruba/cucumber'
 PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..', '..')).freeze
 
 Before do
-  @aruba_timeout_seconds = 60
+  @aruba_timeout_seconds = 3600
 end
