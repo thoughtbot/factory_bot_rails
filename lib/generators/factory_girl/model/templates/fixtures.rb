db36d2e9cc563e0b8eee339bb4e714e68150d6db
Alex Slynko <asl@ukrgermanenterprise.com>
2011-12-19 00:53:20 +0200

    add class name for factories generated from model with module

--- lib/generators/factory_girl/model/templates/fixtures.rb
+++ lib/generators/factory_girl/model/templates/fixtures.rb
@@ -1,7 +1,7 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :<%= singular_name %> do
+  factory :<%= singular_name %><%= ", :class = > #{class_name}" if class_path.present? %> do
 <% for attribute in attributes -%>
     <%= attribute.name %> <%= attribute.default.inspect %>
 <% end -%>
