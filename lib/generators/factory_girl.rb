7df9c9b13a41f3410d979032aaff6831397f2c6f
Joshua Flanagan <joshuaflanagan@gmail.com>
2012-02-26 12:58:48 -0600

    Respect model namespaces when generating factories.
    The factory name for the model Namespaced::User wil be :namespaced_user.
    The :class option is specified as a string to prevent class reloading issues.
    
    Addresses GH-44

--- lib/generators/factory_girl.rb
+++ lib/generators/factory_girl.rb
@@ -6,6 +6,10 @@ module FactoryGirl
       def self.source_root
         @_factory_girl_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'factory_girl', generator_name, 'templates'))
       end
+
+      def explicit_class_option
+        ", :class => '#{class_name}'" unless class_name == singular_table_name.camelize
+      end
     end
   end
 end
