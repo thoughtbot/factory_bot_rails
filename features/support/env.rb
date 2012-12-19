b34a55450668628a253e2e5618bb88623c6fa9dd
Matt Jankowski <mjankowski@thoughtbot.com>
2012-12-19 14:29:51 -0500

    Speed up performance on jruby

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,3 +5,10 @@ PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..', '..')).f
 Before do
   @aruba_timeout_seconds = 3600
 end
+
+Aruba.configure do |config|
+  config.before_cmd do |cmd|
+    set_env('JRUBY_OPTS', "-X-C #{ENV['JRUBY_OPTS']}") # disable JIT since these processes are so short lived
+    set_env('JAVA_OPTS', "-d32 #{ENV['JAVA_OPTS']}") # force jRuby to use client JVM for faster startup times
+  end
+end if RUBY_PLATFORM == 'java'
