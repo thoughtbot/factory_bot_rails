0c3150f3293826931aee97b9341810869136585f
Iazel <iazel.datenshi@gmail.com>
2013-02-19 04:31:54 +0100

    Update lib/factory_girl_rails/railtie.rb
    Now is possible to disable factory_girls through configurations:
    ```ruby
    config.generators.factory_girl false
    ```
    
    Also, it support generators based on rspec or that use the same mechanism, and also the test_unit one.

--- lib/factory_girl_rails/railtie.rb
+++ lib/factory_girl_rails/railtie.rb
@@ -7,18 +7,12 @@ module FactoryGirl
     initializer "factory_girl.set_fixture_replacement" do
       generators = config.respond_to?(:app_generators) ? config.app_generators : config.generators
       rails_options = generators.options[:rails]
+            
+      next if rails_options[:factory_girl] == false
 
-      if rails_options[:test_framework] == :rspec
-        factory_girl_dir = generators.options.fetch(:factory_girl, { :dir => 'spec/factories' })[:dir]
-
-        if rails_options.has_key?(:fixture_replacement)
-          generators.fixture_replacement rails_options[:fixture_replacement], :dir => factory_girl_dir
-        else
-          generators.fixture_replacement :factory_girl, :dir => factory_girl_dir
-        end
-      else
-        generators.test_framework rails_options[:test_framework], :fixture => false, :fixture_replacement => :factory_girl
-      end
+      generators.test_framework rails_options[:test_framework], :fixture => false, :fixture_replacement => :factory_girl
+      factory_girl_dir = generators.options.fetch(:factory_girl, { :dir => 'spec/factories' })[:dir]
+      generators.fixture_replacement :factory_girl, :dir => factory_girl_dir
     end
 
     initializer "factory_girl.set_factory_paths" do
