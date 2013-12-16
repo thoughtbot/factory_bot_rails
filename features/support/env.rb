8f4823435683b57b048aa2d4823ecf57920b99d6
Joshua Clayton <jclayton@thoughtbot.com>
2013-12-16 08:35:06 -0500

    Optimize for JRuby 32/64 bit differences

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,6 +9,13 @@ end
 Aruba.configure do |config|
   config.before_cmd do |cmd|
     set_env('JRUBY_OPTS', "-X-C #{ENV['JRUBY_OPTS']}") # disable JIT since these processes are so short lived
-    set_env('JAVA_OPTS', "-d32 #{ENV['JAVA_OPTS']}") # force jRuby to use client JVM for faster startup times
+
+    java_options = ENV['JAVA_OPTS']
+
+    if 1.size == 4 # 4 for 32 bit java, 8 for 64 bit java.
+      set_env('JAVA_OPTS', "-d32 #{java_options}")
+    else
+      set_env('JAVA_OPTS', "-XX:+TieredCompilation -XX:TieredStopAtLevel=1 #{java_options}")
+    end
   end
 end if RUBY_PLATFORM == 'java'
