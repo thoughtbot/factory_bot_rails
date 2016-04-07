9e0b178c6ff0c229c09a7a71119767c1f474ea74
Joshua Clayton <joshua.clayton@gmail.com>
2016-04-07 16:23:39 -0400

    Correctly configure Aruba command timeout
    This removes the misconfiguration of Aruba timeout by instance variable

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,8 +2,8 @@ require 'aruba/cucumber'
 
 PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..', '..')).freeze
 
-Before do
-  @aruba_timeout_seconds = 3600
+Aruba.configure do |config|
+  config.exit_timeout = Integer ENV.fetch("ARUBA_TIMEOUT") { 120 }
 end
 
 Aruba.configure do |config|
