122ccb3870c8cc5a3479058171ee59ed4c91acb5
Daniel Colson <danieljamescolson@gmail.com>
2019-04-05 11:19:56 -0400

    Write a failing test for reloading with Spring
    This test captures what I think is going on in
    https://github.com/thoughtbot/factory_bot_rails/issues/323
    
    Currently the factory_bot_rails reloader only reloads factory
    definitions when the definition files have changes (using
    `execute_if_updated`).
    
    This can cause problems when editing an application file without also
    editing a factory_bot definition file. Rails will reload the entire
    application, but will not reload the definitions. This causes
    factory_bot to have references to classes that were unloaded from the
    Rails application and replaced with new classes.
    
    https://github.com/thoughtbot/factory_bot_rails/pull/329 replaces
    `execute_if_updated` with `execute` so that factory_bot_rails will
    reload the definition files whenever anything in the application is
    changed. Hopefully that PR will get this test to pass!

--- features/step_definitions/appraisal.rb
+++ features/step_definitions/appraisal.rb
@@ -3,3 +3,9 @@ When /^I run `([^"]+)` with a clean environment$/ do |command|
     I successfully run `ruby -e 'system({"BUNDLE_GEMFILE" => nil, "DISABLE_SPRING" => "true"}, "#{command}")'`
   STEP
 end
+
+When /^I run `([^"]+)` with Spring enabled$/ do |command|
+  step <<~STEP
+    I successfully run `ruby -e 'system({"BUNDLE_GEMFILE" => nil}, "#{command}")'`
+  STEP
+end
