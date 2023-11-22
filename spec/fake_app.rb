e2e1bdbef43d7e3407642c28cc4246702a29c944
Benoit Tigeot <benoit.tigeot@lifen.fr>
2023-11-22 21:46:03 +0100

    Prevent warning about deprecated cache_format_version

--- spec/fake_app.rb
+++ spec/fake_app.rb
@@ -4,6 +4,10 @@ module Dummy
   class Application < Rails::Application
     config.eager_load = false
     config.root = "spec/fixtures"
+
+    if Rails.gem_version >= Gem::Version.new("7.1")
+      config.active_support.cache_format_version = 7
+    end
   end
 end
 
