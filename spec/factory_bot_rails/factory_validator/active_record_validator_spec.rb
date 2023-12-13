38f75b40c3c8b4c4d5ac763cb2233207acbc044d
Charles Maresh <zorab47@gmail.com>
2023-12-13 09:45:08 -0600

    Allow factories to skip primary key definition checks
    Only a proof of concept idea.

--- spec/factory_bot_rails/factory_validator/active_record_validator_spec.rb
+++ spec/factory_bot_rails/factory_validator/active_record_validator_spec.rb
@@ -19,6 +19,44 @@ describe FactoryBotRails::FactoryValidator do
         expect { FactoryBot.create(:article) }
           .to raise_error(FactoryBot::AttributeDefinitionError)
       end
+
+
+      context "when factory is configured to allow primary key definitions" do
+        # NOTE: Modifying Factory here is only a proof of concept
+        class FactoryBot::Factory
+          attr_reader :options
+
+          def initialize(name, options = {})
+            assert_valid_options(options)
+            @name = name.respond_to?(:to_sym) ? name.to_sym : name.to_s.underscore.to_sym
+            @parent = options[:parent]
+            @aliases = options[:aliases] || []
+            @class_name = options[:class]
+            @definition = FactoryBot::Definition.new(@name, options[:traits] || [])
+            @compiled = false
+
+            @options = options # <-- Addition
+          end
+
+          def assert_valid_options(options)
+            options.assert_valid_keys(:class, :parent, :aliases, :traits, :config)
+          end
+        end
+
+        it "does not raise an error for a sequence generating its primary key" do
+          define_model "Warehouse", an_id: :integer do
+            self.primary_key = :an_id
+          end
+
+          FactoryBot.define do
+            factory :warehouse, config: { allow_primary_key_definitions: true } do
+              sequence(:an_id)
+            end
+          end
+
+          expect { FactoryBot.create(:warehouse) }.not_to raise_error
+        end
+      end
     end
   end
 end
