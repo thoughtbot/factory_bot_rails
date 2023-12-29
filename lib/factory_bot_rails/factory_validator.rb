c4638f6b62f580a9e3876e9b80dab4fe7bc5fe22
Mike Burns <mburns@thoughtbot.com>
2023-12-29 18:46:45 -0500

    Remove code path for Rails 5.2 support

--- lib/factory_bot_rails/factory_validator.rb
+++ lib/factory_bot_rails/factory_validator.rb
@@ -15,24 +15,8 @@ module FactoryBotRails
     private
 
     def validate_compiled_factory
-      if Rails.version >= "6.0"
-        rails_6_0_support
-      else
-        rails_5_2_support
-      end
-    end
-
-    def rails_6_0_support
       proc do |event|
         @validators.each { |validator| validator.validate!(event.payload) }
-      end
-    end
-
-    def rails_5_2_support
-      proc do |*notification_event_arguments|
-        event = ActiveSupport::Notifications::Event.new(*notification_event_arguments)
-
-        rails_6_0_support.call(event)
       end
     end
   end
